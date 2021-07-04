43b4f4a002d1568cab8c98feceec36703b974966
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2021-07-04 16:37:52 +1000

    fix logo reading

--- bin/README.md.sh
+++ bin/README.md.sh
@@ -114,7 +114,7 @@ fi
 # add logo.
 pattern="%LOGO%"
 if [[ $template == *"$pattern"* ]]; then
-  logo=$(find docs/ -name 'logdo.*')
+  logo=$(find docs/ -name 'logo.*')
   if [[ -z "$logo" ]]; then
     template=$(<<< "$template" sed "/$pattern/,+1 d")
   else
