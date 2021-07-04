fb009571d6f94ac592b6acdeedcd59031782673f
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2021-07-04 16:17:13 +1000

    start implementing the template-cleanup

--- bin/template-cleanup.sh
+++ bin/template-cleanup.sh
@@ -1,5 +1,56 @@
 #!/usr/bin/env bash
+# cleans up a child repository, removing unused files from the parent template.
 
-echo "hello world"
-# TODO
-# TODO add branch protection rule for default branch.
+# funcs.
+die() { echo "$1" >&2; exit "${2:-1}"; }
+
+# check pwd.
+[[ ! -d .git ]] \
+  && die "must be run from repository root directory"
+
+# check deps.
+deps=(rm)
+for dep in "${deps[@]}"; do
+  hash "$dep" 2>/dev/null || missing+=("$dep")
+done
+if [[ ${#missing[@]} -ne 0 ]]; then
+  [[ ${#missing[@]} -gt 1 ]] && { s="s"; }
+  die "missing dep${s}: ${missing[*]}"
+fi
+
+# vars.
+repo="$(basename "$PWD")" \
+    || die "failed to get repository name"
+
+# determine "things" to remove.
+files=(
+  "bin/dispatch.sh"
+  "bin/template-cleanup.sh"
+  "docs"
+)
+if [[ "$repo" == *"-template"* ]]; then
+  files=(
+    "docs"
+  )
+fi
+
+# remove "things".
+for thing in "${files[@]}"; do
+  found=false
+  [[ -d "$thing" ]] && { found=true; }
+  [[ -f "$thing" ]] && { found=true; }
+  [[ $found == false ]] \
+    && die "failed to find $thing; does it exist?"
+  echo rm -rf "$thing" \
+    || die "failed to remove $thing"
+done
+
+
+  # # while we're here, update all the files with
+  # # the new template name.
+
+  # files=$(find . -type f \
+  #   -not -path "$0" \
+  #   -not -path "./.git/*" \
+  #   -not -name '*dispatch.*' \
+  #   -not -name '*template-cleanup.*')
--- bin/update-name.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# updates the parent template name, so that a child repository
-# can become a parent template.
-
-echo "hello world"
-# TODO
