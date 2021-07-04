cccd5772dc56d011df182184f835dcdb4ea34546
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2021-07-04 15:53:57 +1000

    add some todos

--- bin/template-cleanup.sh
+++ bin/template-cleanup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "hello world"
-
+# TODO
 # TODO add branch protection rule for default branch.
---
+++ bin/update-name.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# updates the parent template name, so that a child repository
+# can become a parent template.
+
+echo "hello world"
+# TODO
