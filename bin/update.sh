ed08aee1b3c7b9de32e22e1b55bd81edab79d364
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2021-07-04 18:45:33 +1000

    clean up the update.sh script

--- bin/update.sh
+++ bin/update.sh
@@ -9,25 +9,25 @@ die() { echo "$1" >&2; exit "${2:-1}"; }
 [[ ! -d .git ]] \
   && die "must be run from repository root directory"
 
-# fetch template changes.
-git fetch template \
-  || die "failed to fetch template changes"
+# vars.
+branch="template"
+remoteBranch="$branch/main"
 
-# setup branches.
-remotebranch="template/main"
-branch=$(git branch --show-current) \
-  || die "failed to get current checked out branch"
+# add template as parent, if not found.
+if [[ $(git remote show $branch 2>/dev/null) ]]; then
+  git remote add "$branch" "https://github.com/jmpa-oss/root-template.git" \
+    || die "failed to add remote $branch"
+fi
+
+# fetch changes from parent.
+git fetch "$branch" \
+  || die "failed to fetch changes from "
 
 # check for any changes.
-mapfile -t files < <(git diff --name-only "$branch" "$remotebranch") \
-  || die "failed to get remote changed files list for $remotebranch"
-[[ ${#files[@]} -eq 0 ]] \
-  && die "no files found to update for $remotebranch, skipping merge to $branch" 0
+[[ -z $(git status --porcelain) ]] && \
+  die "no changes found from $remoteBranch; skipping merge"
 
-# merge changes.
-# TODO if this fails, should it make a PR in GitHub as a backup?
-echo "##[group]Merging $remotebranch changes to $branch"
-git merge "$remotebranch" --allow-unrelated-histories \
-  -m "update $branch with latest changes from $remotebranch" \
-  || die "failed to merge $remotebranch changes to $branch"
-echo "##[endgroup]"
+# merge.
+git merge "$remoteBranch" --allow-unrelated-histories \
+  -m "update $branch with latest changes from $remoteBranch" \
+  || die "failed to merge $remoteBranch changes to $branch"
