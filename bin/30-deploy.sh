fb64919f497fa02e167bde8aee30e98d0f417ae3
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2022-04-01 21:32:41 +1100

    update 30-deploy.sh to use list-repository-topics.sh

--- bin/30-deploy.sh
+++ bin/30-deploy.sh
@@ -19,20 +19,34 @@ if [[ ${#missing[@]} -ne 0 ]]; then
   die "missing dep${s}: ${missing[*]}"
 fi
 
-# check auth.
-aws sts get-caller-identity &>/dev/null \
-  || die "unable to connect to AWS; are you authed?"
-
-# check template.
+# check args.
 name="$1"
 [[ -z "$name" ]] && usage
+if [[ $name == cf/*.yml ]]; then
+  name=${name/cf\//}
+  name=${name/\.yml/}
+fi
+
+# check template.
 template="cf/$name.yml"
-[[ -f "$template" ]] || die "missing $template"
+[[ -f "$template" ]] || \
+  die "missing $template"
 
 # vars.
 project=$(basename "$PWD") \
   || die "failed to get project name"
 stack="$project-$name"
+
+# check auth.
+aws sts get-caller-identity &>/dev/null \
+  || die "unable to connect to AWS; are you authed?"
+
+# retrieve repository topics.
+topics=$(bin/00-list-repository-topics.sh "$GITHUB_REPOSITORY") \
+  || die "failed to list repository topics"
+
+echo "$topics"
+exit 0
 
 # deploy stack.
 echo "##[group]Deploying $template"
