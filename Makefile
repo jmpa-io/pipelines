7626fa9b8a5e5fe63d6cd9933a060fb95774ab53
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2023-12-27 21:49:15 +1100

    update makefile to be able to build binaries properly

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
-PROJECT = roots
+# default PROJECT, if not given by another Makefile.
+ifndef PROJECT
+PROJECT=roots
+endif
 
 ---: ## ---
 
-# include common targets.
+# includes the common Makefile.
+# PLEASE NOTE: this recursively goes back and finds the `.git` directory and
+# assumes this is the root of the project.
 include $(shell while [[ ! -d .git ]]; do cd ..; done; pwd)/Makefile.common.mk
--- Makefile.common.mk
+++ Makefile.common.mk
@@ -2,42 +2,61 @@
 # This Makefile is generic across all repositories:
 # https://github.com/jmpa-io/root-template/blob/main/Makefile.common.mk
 #
+# See the bottom for additional notes.
+#
 
 # Check project variables are given.
 ifndef PROJECT
-$(error PROJECT not defined, missing Makefile?)
+$(error PROJECT not defined, missing from Makefile?)
 endif
 
-# Vars.
-SHELL           = /bin/sh
-ENVIRONMENT     ?= dev
-COMMIT          = $(shell git describe --tags --always)
-REPO            = $(shell basename $(shell git rev-parse --show-toplevel))
-FILE_SIZE		= $(shell stat -f '%z' $<)
+# Variables.
+SHELL          					= /bin/sh # The shell to use for executing commands.
+ENVIRONMENT     				?= dev # The environment to deploy to.
+COMMIT          				= $(shell git describe --tags --always) # The git commit hash.
+REPO            				= $(shell basename $(shell git rev-parse --show-toplevel)) # The name of the repository.
+COMMA							:= , # Used for if conditions in Make where a comma is needed.
+OS 								:= $(shell uname | tr '[:upper:]' '[:lower:]') # The operating system the Makefile is being executed on.
+BUILDING_OS 					?= $(OS) # The operating system used when building binaries.
+SUPPORTED_OPERATING_SYSTEMS 	= linux,darwin # A list of operating systems that can be used when building binaries.
 
-# Files.
-SH_FILES        := $(shell find . -name "*.sh" -type f 2>/dev/null)
-GO_FILES        := $(shell find . -name "*.go" -type f 2>/dev/null)
-CF_FILES        := $(shell find ./cf -name 'template.yml' -type f 2>/dev/null)
-SAM_FILES       := $(shell find ./cf -name 'template.yaml' -type f 2>/dev/null)
-CF_DIRS         := $(shell find ./cf -mindepth 1 -maxdepth 1 -type d 2>/dev/null)
-CMD_DIRS        := $(shell find ./cmd -mindepth 1 -maxdepth 1 -type d 2>/dev/null)
-IMAGES          := $(patsubst .,$(PROJECT),$(patsubst ./%,%,$(shell find . -name 'Dockerfile' -type f -exec dirname {} \; 2>/dev/null)))
+# Setup OS specific variables.
+ifeq ($(OS),linux)
+	SED_FLAGS 	= -i
+	FILE_SIZE 	= $(shell stat -c '%s' $<)
+else ifeq ($(OS),darwin)
+	SED_FLAGS 	= -i ''
+	FILE_SIZE	= $(shell stat -f '%z' $<)
+endif
+
+# Files + Directories.
+SH_FILES    := $(shell find . $(IGNORE_SUBMODULES) -name "*.sh" -type f 2>/dev/null)
+GO_FILES	:= $(shell find . $(IGNORE_SUBMODULES) -name "*.go" -type f 2>/dev/null)
+CF_FILES    := $(shell find ./cf $(IGNORE_SUBMODULES) -name 'template.yml' -type f 2>/dev/null)
+SAM_FILES	:= $(shell find ./cf $(IGNORE_SUBMODULES) -name 'template.yaml' -type f 2>/dev/null)
+CF_DIRS     := $(shell find ./cf $(IGNORE_SUBMODULES) -mindepth 1 -maxdepth 1 -type d 2>/dev/null)
+CMD_DIRS    := $(shell find ./cmd $(IGNORE_SUBMODULES) -mindepth 1 -maxdepth 1 -type d 2>/dev/null)
+IMAGES      := $(patsubst .,$(PROJECT),$(patsubst ./%,%,$(shell find . -name 'Dockerfile' -type f -exec dirname {} \; 2>/dev/null)))
+
+# Submodules.
+SUBMODULES 			:= $(shell git config --file $(shell while [[ ! -d .git ]]; do cd ..; done; pwd)/.gitmodules --get-regexp path | awk '{ print $$2 }')
+IGNORE_SUBMODULES 	= $(foreach module,$(SUBMODULES),-not \( -path "./$(module)" -o -path "./$(module)/*" \))
 
 # Binaries.
-CMD_SOURCE      = $(shell find cmd/$* -type f 2>/dev/null)
-BINARIES        = $(patsubst %,dist/%,$(shell find cmd/* -maxdepth 0 -type d -exec basename {} \; 2>/dev/null))
-BINARIES_LINUX  = $(patsubst %,%-linux,$(BINARIES))
+CMD_DIRS 					= $(shell find cmd/* -name main.go -maxdepth 1 -type f -exec dirname {} \; 2>/dev/null | awk -F/ '{$$1=""; sub(/^ /, ""); print $$0}')
+CMD_SOURCE 					= $(addprefix cmd/,$(CMD_DIRS))
+BINARIES_TARGETS 			= $(addprefix binary-,$(CMD_DIRS))
+BINARIES_OUTPUT_DIRECTORIES = $(addprefix dist/,$(CMD_DIRS))
 
 # AWS.
 AWS_REGION      ?= ap-southeast-2
 STACK_NAME      = $(PROJECT)-$*
-AWS_ACCOUNT_ID	= $(shell aws sts get-caller-identity --query 'Account' --output text)
+AWS_ACCOUNT_ID	?= $(shell aws sts get-caller-identity --query 'Account' --output text)
 ECR             = $$(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com
-BUCKET			= jmpa-io-artifacts
+BUCKET			?= jmpa-io-artifacts
 
 # Funcs.
-get_last_element = $(lastword $(subst /, ,$1)) # Splits a string by '/' and retrieves the last element in the array.
+get_last_element = $(lastword $(subst /, ,$1)) # Splits a string by '/' and retrieves the last element in the given array.
 
 # Check deps.
 ifndef CI
@@ -72,17 +91,17 @@ lint-go: ## Lints Go files.
 ifeq ($(GO_FILES),)
 	@echo "No *.go files to lint."
 else
-	golangci-lint run -v --allow-parallel-runners
+	golangci-lint run -v --allow-parallel-runners ./...
 endif
 	@test -z "$(CI)" || echo "##[endgroup]"
 
 .PHONY: lint-cf
 lint-cf: ## Lints CF templates.
-	@test -z "$(CI)" || echo "##[group]Linting cf."
+	@test -z "$(CI)" || echo "##[group]Linting Cloudformation."
 ifeq ($(CF_FILES),)
 	@echo "No ./cf/*/template.yml files to lint."
 else
-	find ./cf -type f -name 'template.yml' -exec cfn-lint -r $(AWS_REGION) -t '{}' \; || true
+	find ./cf -type f -name 'template.yml' -exec sh -c 'cfn-lint -r $(AWS_REGION) -t "{}" && aws cloudformation validate-template --template-body file://{}' \;
 endif
 	@test -z "$(CI)" || echo "##[endgroup]"
 
@@ -114,52 +133,92 @@ endif
 test: test-go ## ** Tests everything.
 
 .PHONY: test-go
-test-go: dist ## Tests Go + generates coverage report.
+test-go: dist/coverage.txt ## Runs Go tests.
+dist/coverage.txt: dist
 	@test -z "$(CI)" || echo "##[group]Unit tests."
 ifeq ($(GO_FILES),)
 	@echo "No *.go files to test."
 else
 	@go version
-	CGO_ENABLED=1 go test -short -coverprofile=dist/coverage.txt \
-		-covermode=atomic -race -vet=off ./... \
-		&& go tool cover -func=dist/coverage.txt
+	CGO_ENABLED=1 go test -short -coverprofile=$@ \
+    	-covermode=atomic -race -vet=off ./...
 endif
 	@test -z "$(CI)" || echo "##[endgroup]"
+
+.PHONY: code-coverage
+code-coverage: dist/coverage.txt ## Generates a Go code coverage report, broken-down by function, to stdout.
+	@if [[ -f $< ]]; then \
+		test -z "$(CI)" || echo "##[group]Code coverage."; \
+		go tool cover -func=$<; \
+		test -z "$(CI)" || echo "##[endgroup]"; \
+	fi
+
+.PHONY: code-coverage-html
+code-coverage-html: dist/coverage.txt ## Generates a Go code HTML coverage report, rendered in the default browser.
+	@if [[ -f $< ]]; then \
+		go tool cover -html=$<; \
+	fi
 
 # ┌┐ ┬┌┐┌┌─┐┬─┐┬┌─┐┌─┐
 # ├┴┐││││├─┤├┬┘│├┤ └─┐
 # └─┘┴┘└┘┴ ┴┴└─┴└─┘└─┘
 
-.PHONY: binaries
-binaries: $(BINARIES) ## ** Builds ALL binaries.
+.PHONY: binaries binaries-all
+binaries: $(BINARIES_TARGETS) ## ** Builds ALL binaries for the $(BUILDING_OS) environment.
+binaries-all:  ## ** Builds ALL binaries for ALL supported operating systems.
+	@for os in $(shell echo $(SUPPORTED_OPERATING_SYSTEMS) | tr ',' ' '); do \
+		$(MAKE) --no-print-directory BUILDING_OS=$$os binaries; \
+	done
 
-## Creates the dist directory, if missing.
+# Creates the root output directory.
 dist:
-	@mkdir dist
+	@mkdir -p dist
 
-## Builds the binary for the given service (for the local machine platform - GOOS).
-binary-%: dist/%
-dist/%: $(CMD_SOURCE)
-	@test -z "$(CI)" || echo "##[group]Building $@"
+# Creates the output directory, for a given service.
+.SECONDARY: $(BINARIES_OUTPUT_DIRECTORIES)
+dist/%: dist
+	@mkdir -p dist/$*
+
+# Builds a binary, for the given service, for the Linux environment.
+define build_binary_linux
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
+	go build --trimpath \
+		-tags lambda.norpc \
+		-ldflags "-w -s -X version.Version=$(COMMIT)" \
+		-o dist/$*/$*-linux-amd64 ./cmd/$*
+endef
+
+# Builds a binary, for the given service, for the Darwin environment.
+define build_binary_darwin
+	CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 \
+	go build --trimpath \
+		-ldflags "-w -s -X version.Version=$(COMMIT)" \
+		-o dist/$*/$*-darwin-arm64 ./cmd/$*
+endef
+
+# A wrapper for building a binary, for the given service, for the $(BUILDING_OS) environment.
+define build_binary
+	@echo "##[group]Building $* binary for $1."
 	@go version
-	go build --trimpath -ldflags "-w -s \
-		    -X version.Version=$(COMMIT)" \
-		    -o $@ ./cmd/$*
+	$(call build_binary_$1)
 	@test -z "$(CI)" || echo "##[endgroup]"
+endef
 
-## Builds the binary for the given service (for Linux).
-binary-%-linux: dist/%-linux
-dist/%-linux: $(CMD_SOURCE)
-	@test -z "$(CI)" || echo "##[group]Building $@"
-	@go version
-	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build --trimpath -ldflags "-w -s \
-		    -X version.Version=$(COMMIT)" \
-		    -o $@ ./cmd/$*
-	@test -z "$(CI)" || echo "##[endgroup]"
+## Builds a binary, for the given service, for the $(BUILDING_OS) environment.
+binary-%: cmd/%/main.go dist/%
+	$(call build_binary,$(BUILDING_OS))
 
-## Zips the given service.
-dist/%.zip: dist/%-linux
-	@zip -rjDm $@ dist/$*-linux
+# ┬  ┌─┐┌┬┐┌┐ ┌┬┐┌─┐
+# │  ├─┤│││├┴┐ ││├─┤
+# ┴─┘┴ ┴┴ ┴└─┘─┴┘┴ ┴
+
+# Moves the bootstrap, for the given service, into the dist directory.
+bootstrap-%: dist/% cmd/%/bootstrap
+	@cp cmd/$*/bootstrap dist/$*/
+
+# Invokes the given service locally, if able.
+invoke-%: cmd/%/local.sh binary-% bootstrap-%
+	@$<
 
 # ┌┬┐┌─┐┌─┐┬┌─┌─┐┬─┐
 #  │││ ││  ├┴┐├┤ ├┬┘
@@ -217,31 +276,34 @@ auth-aws: ## Checks current auth to AWS; An error indicates an issue with auth t
 # Sets PRIMARY_SERVICES to be SERVICES, so you can use either in a Makefile.
 PRIMARY_SERVICES ?= $(SERVICES)
 
-.PHONY: deploy $(PRIMARY_SERVICES) $(SECONDARY_SERVICES) $(TERTIARY_SERVICES)
-deploy: $(PRIMARY_SERVICES) $(SECONDARY_SERVICES) $(TERTIARY_SERVICES) ## ** Deploys the Cloudformation template for ALL services.
+.PHONY: deploy $(PRIMARY_SERVICES) $(SECONDARY_SERVICES) $(TERTIARY_SERVICES) $(QUATERNARY_SERVICES) $(QUINARY_SERVICES)
+deploy: $(PRIMARY_SERVICES) $(SECONDARY_SERVICES) $(TERTIARY_SERVICES) $(QUATERNARY_SERVICES) $(QUINARY_SERVICES) ## ** Deploys the Cloudformation template for ALL services.
 
 ## Deploys the Cloudformation template for the given service.
 deploy-%: cf/%/package.yml
+ifndef ENVIRONMENT
+	$(error ENVIRONMENT not defined; please populate it before deploying)
+else
 	@test -z "$(CI)" || echo "##[group]Deploying $*."
 	aws cloudformation deploy \
 		--region $(AWS_REGION) \
 		--template-file $< \
 		$(shell [[ $(FILE_SIZE) -ge 51200 ]] && echo "--s3-bucket $(BUCKET)") \
 		--stack-name $(STACK_NAME) \
-		--tags \
-			repository=$(REPO) \
-			project=$(PROJECT) \
-			component=$* \
-			revision=$(COMMIT) \
-		--capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+		--tags repository=$(REPO) project=$(PROJECT) component=$* revision=$(COMMIT) \
+		$(if $(ADDITIONAL_STACK_TAGS),$(ADDITIONAL_STACK_TAGS),) \
+		--capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
 		--parameter-overrides Component=$* Revision=$(COMMIT) Environment=$(ENVIRONMENT) \
-		$(if $(wildcard cf/params/$(ENVIRONMENT).json),$(shell jq -r 'map("\(.ParameterKey)=\(.ParameterValue)") | join(" ")' ./cf/params/$(ENVIRONMENT).json),) \
+		$(if $(wildcard cf/.params/$(ENVIRONMENT).json),$(shell jq -r 'map("\(.ParameterKey)=\(.ParameterValue)") | join(" ")' ./cf/.params/$(ENVIRONMENT).json),) \
 		$(if $(ADDITIONAL_PARAMETER_OVERRIDES),$(ADDITIONAL_PARAMETER_OVERRIDES),) \
 		--no-fail-on-empty-changeset
-	@test -z "$(CI)" || echo "##[endgroup]"
+endif
 
 ## Packages the Cloudformation template for the given service.
 cf/%/package.yml: cf/%/template.yml
+ifndef ENVIRONMENT
+	$(error ENVIRONMENT not defined; please populate it before deploying)
+else
 	@test -z "$(CI)" || echo "##[group]Packaging $*."
 	aws cloudformation package \
 		--region $(AWS_REGION) \
@@ -250,6 +312,7 @@ cf/%/package.yml: cf/%/template.yml
 		--s3-prefix $(PROJECT) \
 		--s3-bucket $(BUCKET)
 	@test -z "$(CI)" || echo "##[endgroup]"
+endif
 
 # ┌┬┐┬┌─┐┌─┐
 # ││││└─┐│
@@ -262,7 +325,7 @@ generate-readme: ## Generates a README.md, using a template.
 .PHONY: update-template
 update-template: ## Pulls changes from root-template into this repository.
 	git fetch template
-	git merge template/master --allow-unrelated-histories
+	git merge template/main --allow-unrelated-histories
 
 .PHONY: clean
 clean: ## Removes generated files and folders, resetting this repository back to its initial clone state.
@@ -309,12 +372,9 @@ list-cmd: # Lists ALL dirs under ./cmd.
 	@echo $(CMD_DIRS)
 
 .PHONY: list-binaries
-list-binaries: # Lists ALL binaries.
-	@echo $(BINARIES)
-
-.PHONY: list-binaries-linux
-list-binaries-linux: # Lists ALL Linux binaries.
-	@echo $(BINARIES_LINUX)
+list-binaries: # Lists ALL binary targets and their output directories.
+	@echo $(BINARIES_TARGETS)
+	@echo $(BINARIES_OUTPUT_DIRECTORIES)
 
 .PHONY: list-images
 list-images: # Lists ALL docker images for every service.
@@ -325,3 +385,15 @@ list-deploy: # Lists ALL services to deploy.
 	@echo $(PRIMARY_SERVICES)
 	@echo $(SECONDARY_SERVICES)
 	@echo $(TERTIARY_SERVICES)
+	@echo $(QUATERNARY_SERVICES)
+	@echo $(QUINARY_SERVICES)
+
+.PHONY: list-deploy
+list-submodules: # Lists ALL submodules.
+	@echo $(SUBMODULES)
+
+# ┌┐┌┌─┐┌┬┐┌─┐┌─┐
+# ││││ │ │ ├┤ └─┐
+# ┘└┘└─┘ ┴ └─┘└─┘
+
+# ASCII art in this file are generated from: https://patorjk.com/software/taag/#p=display&h=0&v=0&f=Calvin%20S&t=notes%0A
